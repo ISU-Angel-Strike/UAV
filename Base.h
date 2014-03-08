e38db6f797056acf55611a6b04c0e566c3f2e53d
cwenger91 <cjwenger91@gmail.com>
2014-03-08 13:14:22 -0600

    Update Base.h

--- Base.h
+++ Base.h
@@ -1,19 +1,22 @@
+
 #define		ST_IDLE		1
 #define		ST_ID		2
 #define		ST_LENGTH	3
 #define		ST_DATA		4
-#define     	packStart	55
+#define     packStart		55
 class Base
 {
 public:
 		Base();
 		byte* GetData(byte id, byte* l);
 		Send(byte id, byte length,byte data);
-        init(byte id, byte length);
-        State(byte currentState, byte id, byte length, byte data);
+        	init(byte id, byte length);
+        	State(byte currentState, byte id, byte length, byte data);
+		void Stock(byte id, byte* data);
 private:
 		byte length[8];
 		byte data[8][128];
+		byte dataStock[8];
 		byte id[8];
 		byte currentState;
 		byte idNum;
