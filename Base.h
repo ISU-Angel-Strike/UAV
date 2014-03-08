a74bd6abcc5835cf852e6a8b971357060b5fde9e
cwenger91 <cjwenger91@gmail.com>
2014-03-08 12:00:54 -0600

    Update Base.h

--- Base.h
+++ Base.h
@@ -4,10 +4,13 @@ public:
 		Base();
 		byte* GetData(byte id, byte* l);
 		Send(byte id, byte length,byte data);
+        init(byte id, byte length);
+        State(byte currentState, byte id, byte length, byte data);
 private:
 		byte length[8];
 		byte data[8][128];
 		byte id[8];
+		byte currentState;
 		int numberOfPackages;
 		
 };
