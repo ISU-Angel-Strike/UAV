5c808fc9d8dcc4e804d1e316d14e67c13ab9909b
cwenger91 <cjwenger91@gmail.com>
2014-03-08 12:00:37 -0600

    Update Base.cpp

--- Base.cpp
+++ Base.cpp
@@ -1,3 +1,46 @@
+#include <Base.h>
+
+Base::Base()
+{
+}
+
+//Checks the state for incoming data
+Base::State(byte currentState, byte id, byte length, byte data)
+{
+	currentState = ST_IDLE;
+    switch (currentState) {
+        case ST_IDLE:
+			//Sets the currentState to the next state 
+			currentState = ST_ID;
+            break;
+        case ST_ID:
+			if(Serial.read() > -1){
+			currentState = ST_LENGTH;
+			}else{
+				currentState = ST_IDLE;
+			}
+            break;
+        case ST_LENGTH:
+			//checks the length of the incoming data
+			if(length == 128){
+				currentState = ST_DATA;
+				break;
+			}else{
+				currentState = ST_IDLE;
+				break;
+			}
+        case ST_DATA:
+			GetData(byte id, byte* l);
+			currentState = ST_IDLE;
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+//Sends the data and also sends a checksum to make sure data is not corrupt
 Base::Send(byte id,byte length,byte *data)
 {
    byte checksum = 0;
@@ -9,7 +52,7 @@ Base::Send(byte id,byte length,byte *data)
    }
    Serial.write(checksum);
 }
-
+//copies data from data packages and moves the data data package to the front of the qeue 
 Base::GetData(byte id, byte* l)
 {
 		byte d[128]
