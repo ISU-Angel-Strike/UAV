73eedaa3658e21e78a301c9e954f72cf70def7ed
cwenger91 <cjwenger91@gmail.com>
2014-03-12 19:10:18 -0500

    Update Base.h

--- Base.h
+++ Base.h
@@ -1,9 +1,9 @@
-
 #define		ST_IDLE		1
 #define		ST_ID		2
 #define		ST_LENGTH	3
 #define		ST_DATA		4
-#define     packStart		55
+#define     	ST_CHECK    	5
+#define     	packStart	55
 class Base
 {
 public:
@@ -13,14 +13,18 @@ public:
         	init(byte id, byte length);
         	State(byte currentState, byte id, byte length, byte data);
 		void Stock(byte id, byte* data);
+        	GetStock(byte id);
 private:
-		byte length[8];
 		byte data[8][128];
 		byte dataStock[8];
-		byte id[8];
+		byte tempStock[1][128];
 		byte currentState;
+		byte stData[1][128];
 		byte idNum;
 		byte idCheck;
 		int numberOfPackages;
+        int checkSumState;
+        int step;
+		
 		
 };
