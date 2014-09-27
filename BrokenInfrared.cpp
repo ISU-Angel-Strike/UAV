bd75853fa8ef61e6bbb9bfa9f0d5e68a5167242e
antkhoun <antkhoun@iastate.edu>
2014-09-27 12:40:42 -0500

    Update BrokenInfrared.cpp

--- BrokenInfrared.cpp
+++ BrokenInfrared.cpp
@@ -1,66 +1,24 @@
+// 9/27/14 - Goobertron
+
 #include "Infrared.h"
 #include <math.h>
 #include <Arduino.h>
 
+
 Infrared::Infrared(){
-
 }
 
-bool Infrared::Init(byte id){
-	bool retval = true;
-	
-	running = false;
-	this->id = id;
-	
-	if(id == 0)
-		pin = 2;
-	else if(id == 1)
-		pin = 3;
-	else{
-		this->id = 0;
-		pin = 2;
-		
-		retval = false;
-	}
-	
-	_infrared = this;
-	
-	return retval;
-}
-
-void Infrared::Start(){
-	if(running == false){
-		running = true;
-		
-		attachInterrupt(id, Infrared::InfraredInterrupt, CHANGE);
-	}
-}
 
 float Infrared::Get(){
-	return dist;
+	return DistFilt();
 }
 
-void Infrared::Stop(){
-	if(running == true){
-		running = false;
-		
-		detachInterrupt(id);
-	}
-}
-
-bool Infrared::IsRunning(){
-	return running;
-}
-
-void Infrared::InfraredInterrupt(){
-	_infrared->ProcessInfraredInterrupt();
-}
-
-void Infrared::ProcessInfraredInterrupt(){
+float Infrared::DistFilt(){ 
 	float newDist;
 	//distance (cm) = 27.86 (voltage reading)^-1.15
-	newDist = 27.86*pow(analogRead(pin)*(5.0/1023.0),-1.15);
-	dist = Infrared::Filter(newDist, dist);
+	newDist = 27.86*pow(analogRead(pin)*(5.0/1023.0),-1.15); //where does the 5.0/1023.0 come from?
+	dist = Filter(newDist, dist);
+	return dist;
 }
 
 float Infrared::Filter(float current, float prev){
