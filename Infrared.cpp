ed73092b2ce257ed9e927df0c79869d26ce80f81
antkhoun <antkhoun@iastate.edu>
2014-10-04 13:23:41 -0500

    Update Infrared.cpp

--- Infrared.cpp
+++ Infrared.cpp
@@ -1,10 +1,20 @@
+
 // 9/25/14 - Goobertron
+// 10/4/14
+	//Tested through the arduino sketch and sensor is about 3-5 cm off
+	//for closer calculations. Farther calculations will cause an
+	//'inf' error and the accuracy of the sensor is around ~80 cm.
+	//Anything out of range (10cm-80cm), the output will be 0.
+	//I changed the reading from centimeters to meters.
+	//Sensor - Sharp 2Y0A21
+	// - Khoun
 
 #include "Infrared.h"
 #include <math.h>
 #include <Arduino.h>
 
 Infrared *_infrared; 
+
 
 Infrared::Infrared(){
 }
@@ -18,6 +28,8 @@ float Infrared::DistFilt(){
 	//distance (cm) = 27.86 (voltage reading)^-1.15
 	newDist = 27.86*pow(analogRead(pin)*(5.0/1023.0),-1.15); //where does the 5.0/1023.0 come from?
 	dist = Filter(newDist, dist);
+	if(dist > 80 || dist < 10) dist = 0; //range of 10 cm to 80 cm
+	dist = dist*.01; //converts centimeter into meter
 	return dist;
 }
 
