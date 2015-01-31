6889c6ea6fe376b34a008c01ecf1bbf4f8d80077
EricMiddleton1 <ericm@iastate.edu>
2015-01-31 12:15:15 -0600

    Update TaskQueue.cpp
    Fixed critical bug that caused intermittent crashes.

--- TaskQueue.cpp
+++ TaskQueue.cpp
@@ -1,15 +1,12 @@
 #include "TaskQueue.h"
 
-//Global pointer to the Task Queue
-TaskQueue *_taskQue;
-
 TaskQueue::TaskQueue() {
 }
 
 //Initialize the Task Queue
 void TaskQueue::Init() {
 	length = 0;
-	_taskQue = this;
+	tQue[0] = NULL;
 }
 
 //Returns the length of the queue
@@ -22,7 +19,10 @@ bool TaskQueue::DoNext() {
 	if(length == 0)											//If the queue is empty
 		return 0;											//Return 0
 	else {													//Otherwise
-		tQue[0](cQue[0]);									//Run the first task
+		noInterrupts();
+		
+		Task t = tQue[0];
+		void *c = (void*)cQue[0];
 		
 		length--;
 		
@@ -30,6 +30,11 @@ bool TaskQueue::DoNext() {
 			tQue[i] = tQue[i + 1];
 			cQue[i] = cQue[i + 1];
 		}
+		tQue[length] = NULL;
+		
+		interrupts();
+
+		t(c);
 	}
 }
 
@@ -41,10 +46,12 @@ bool TaskQueue::Clear() {
 //Adds a new task to the queue
 //Task t:	A pointer to a void function(void*)
 //void *c:	A void pointer that will be passed to the function
-//char *id:	Task identifier string (optional)
 byte TaskQueue::Add(Task t, void *c) {
 	if(length >= MAXQUEUE)
 		return 0;
+	else if(t == NULL) {
+		return 0;
+	}
 	else {
 		tQue[length] = t;
 		cQue[length] = c;
