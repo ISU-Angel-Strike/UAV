2e939a6181df0d09ee5d0b7be4d7d25e84340dba
EricMiddleton1 <ericm@iastate.edu>
2015-01-31 12:04:53 -0600

    Update Scheduler.cpp
    Fixed critical bugs and improved reliability.

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -5,35 +5,39 @@ Scheduler *_schedule;
 Scheduler::Scheduler() {
 }
 
-void Scheduler::Init() {
-	//Setting hardware timer registers for a 1ms interrupt
-	TIMSK2 &= ~(1<<TOIE2);
-	TCCR2A &= ~((1<<WGM21) | (1<<WGM20));
-	TCCR2B &= ~(1<<WGM22);
-	ASSR &= ~(1<<AS2);
-	TIMSK2 &= ~(1<<OCIE2A);
-	TCCR2B |= (1<<CS22) | (1<<CS20);
-	TCCR2B &= ~(1<<CS21);
+void Scheduler::Init(TaskQueue *que) {
+	noInterrupts();
 	
-	tInit = 131;
+	//Setting hardware timer registers for a 1ms interrupt
+	TCCR2A = 0;
+	TCCR2B = 0;
+	TCNT2 = 0;
+	
+	OCR2A = 249; // 16 Mhz / (1KHz * 64(prescaler))
+	TCCR2A |= (1 << WGM21); //Turn on CTC mode
+	TCCR2B |= (1 << CS22); //64 prescaler
+	TIMSK2 &= ~(1 << OCIE2A); //Disable interrupt
+	
+	interrupts();
 	
 	length = 0;
 	ticks = 0;
+	
+	this->que = que;
 	
 	_schedule = this;
 }
 
 void Scheduler::Start() {
-	TCNT2 = tInit;
-	TIMSK2 |= (1<<TOIE2);
+	TIMSK2 |= (1<<OCIE2A);
 }
 
 void Scheduler::Stop() {
-	TIMSK2 &= ~(1<<TOIE2);
+	TIMSK2 &= ~(1<<OCIE2A);
 }
 
 bool Scheduler::IsRunning() {
-	return TIMSK2 & (1<<TOIE2);
+	return TIMSK2 & (1<<OCIE2A);
 }
 
 bool Scheduler::AddTask(Task t, void *c, unsigned short time) {
@@ -45,9 +49,6 @@ bool Scheduler::AddTask(Task t, void *c, unsigned short time) {
 		times[length] = time;
 		offset[length] = ticks % time;
 		cList[length++] = c;
-		
-		//Run it for the first time
-		_taskQue->Add(t, c);
 	}
 	
 	return 1;
@@ -59,7 +60,7 @@ bool Scheduler::RemoveTask(Task t) {
 	for(int i = 0; i < length; i++) {
 		if(t == schedule[i]) {
 			found = 1;
-			for(int j = i; j < (length--); j++) {
+			for(int j = i; j < length; j++) {
 				schedule[j] = schedule[j+1];
 				times[j] = times[j+1];
 				offset[j] = offset[j+1];
@@ -68,7 +69,7 @@ bool Scheduler::RemoveTask(Task t) {
 			break;
 		}
 	}
-	
+	length -= found;
 	return found;
 }
 
@@ -76,18 +77,16 @@ void Scheduler::Interrupt() {
 	_schedule->ProcessInterrupt();
 }
 
-void Scheduler::ProcessInterrupt() {
+void Scheduler::ProcessInterrupt() volatile {
 	ticks++;
 	
 	for(int i = 0; i < length; i++) {
 		if((ticks - offset[i]) % times[i] == 0) {
-			_taskQue->Add(schedule[i], cList[i]);
+			que->Add(schedule[i], cList[i]);
 		}
 	}
-	
-	Start();
 }
 
-ISR(TIMER2_OVF_vect) {
+ISR(TIMER2_COMPA_vect) {
 	Scheduler::Interrupt();
 }
